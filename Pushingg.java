aec847f9e7e8eb0f15cc0528481c4b2dce79f5c0
unnatial2005 <unnatilonakar@gmail.com>
2025-06-11 00:36:25 +0530

    codesss

---
+++ Pushingg.java
@@ -0,0 +1,28 @@
+public class Pushingg {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num > 0) {
+            System.out.println("Positive number");
+        }
+        else if (num < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+        
+    }
+}
+// git remote add origin <>
+// git push -u origin main
+//git branch
+// git branch -b main
+//git branch -M main
+// git checkout -b master 
+// add,commit
+//git push origin master
+//git checkout main
+//git merge master
+//git push origin main
+//git commit --allow-empty -m "Initial commit"
+//git remote set-url origin <url>
+//git remote remove origin
